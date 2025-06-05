ec71765dd85372c8cc3a34264626fd49aa015e6e
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-06-05 18:17:37 +0330

    Refactor RootLayout to return children directly and remove Home component

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className='min-h-screen min-w-screen flex flex-col justify-center items-center'>
-      <h1 className='text-3xl'>Hi, I am a test text!</h1>
-    </main>
-  );
-}

3ce7ae1254d295df1ff534fc5fcb7b6624a62445
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-06-06 13:42:08 +0330

    added favicon and nessecary meta data updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Smart Insurance Portal',
+  title: 'Devotel Smart Insurance Portal',
   description: 'An evaluation project for Devotel by Abolfazl Chaman @ abolfazlchaman.com',
 };
 
